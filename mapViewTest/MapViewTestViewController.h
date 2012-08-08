cdce62a02eda2c85c1a3b5b10a4c514ebedf06d7
Jonathan King <jonathanlking@me.com>
2012-08-08 19:28:49 +0100

    First Commit

--- mapViewTest/MapViewTestViewController.h
+++ mapViewTest/MapViewTestViewController.h
@@ -7,7 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
-@interface MapViewTestViewController : UIViewController
+@interface MapViewTestViewController : UIViewController <MKMapViewDelegate>
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
--- mapViewTest/MapViewTestViewController.m
+++ mapViewTest/MapViewTestViewController.m
@@ -9,10 +9,11 @@
 #import "MapViewTestViewController.h"
 
 @interface MapViewTestViewController ()
-
+@property (nonatomic) BOOL isDownloadingData;
 @end
 
 @implementation MapViewTestViewController
+@synthesize mapView;
 
 - (void)viewDidLoad
 {
@@ -20,8 +21,29 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)mapView:(MKMapView *)mapView regionDidChangeAnimated:(BOOL)animated {
+    [self logIfCached];
+}
+
+- (void)mapViewWillStartLoadingMap:(MKMapView *)mapView {
+    _isDownloadingData = TRUE;
+}
+- (void)mapViewDidFinishLoadingMap:(MKMapView *)mapView {
+    _isDownloadingData = FALSE;
+}
+- (void)mapViewDidFailLoadingMap:(MKMapView *)mapView withError:(NSError *)error {
+    _isDownloadingData = FALSE;
+}
+
+- (void)logIfCached {
+    
+    if (_isDownloadingData) NSLog(@"Downloading");
+    else NSLog(@"Is Cached");
+}
+
 - (void)viewDidUnload
 {
+    [self setMapView:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
